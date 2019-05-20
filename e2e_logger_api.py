5fdb38f5b6b246a7c8c436e4d2e113fee3cac920
Avishkar <avgupta@redhat.com>
2019-05-20 13:33:39 +0000

    Modify end to end test context wrapper to log time

--- e2e_logger_api.py
+++ e2e_logger_api.py
@@ -1,35 +1,36 @@
 from flask import Flask
 from flask import request
+from flask import g
+import logging
 
 
 app = Flask(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.WARNING,
+    filename='testruntime.log',
+    filemode='w',
+    datefmt='%Y-%m-%d %H:%M:%S')
+request_context = ""
+
 
 
 @app.route('/setcontext')
 def setcontext():
-    app.g.context = request.args.get("ctx")
-    app.logger.info("Set context to: {}".format(app.g.context))
+    global request_context
+    request_context= request.args.get("ctx")
+    app.logger.warn("Set context to: {}".format(request_context))
     return ('', 200)
 
 
 @app.route('/endcontext')
 def endcontext():
+    global request_context
     context = request.args.get("ctx")
-    if context == app.g.context:
-        app.g.context = ""
-    app.logger.info("Set context to: {}".format(context))
+    if context == request_context:
+        request_context= ""
+    app.logger.warn("End of context: {}".format(context))
     return ('', 200)
-
-
-@app.route('/logcode', methods=['POST'])
-def log_code():
-    log_string = request.json["fn"]
-    app.logger.info(log_string)
-    return ('', 200)
-
-
-def gremlinRestFunction(*args, **kwargs):
-    pass
 
 
 if __name__ == "__main__":
