7b0da27094981b7befc6523068a7f22e785a6580
Avishkar Gupta <avgupta@redhat.com>
2019-09-24 12:53:10 +0000

    Enhancements to origin patch setup script
    Signed-off-by: Avishkar Gupta <avgupta@redhat.com>

--- scripts/openshift-origin-patch.sh
+++ scripts/openshift-origin-patch.sh
@@ -1,12 +1,11 @@
 set -x
-cd $GOPATH/src/github.com/openshift/
+mkdir -p $GOPATH/src/github.com/openshift/ && cd $GOPATH/src/github.com/openshift/
 rm -rf origin
 git clone https://github.com/openshift/origin --branch release-4.1
 cd $PATCHDIR
-patchsource --source-dir=$GOPATH/src/github.com/openshift/origin --code-config-yaml=patch.yaml
+patchsource --source-dir=$GOPATH/src/github.com/openshift/origin --code-config-yaml=patch-template.yaml
 cd $GOPATH/src/github.com/openshift/origin
-cp -rf $GOPATH/src/github.com/maruel/ $GOPATH/src/github.com/openshift/origin/_output/local/go/src/github.com/openshift/origin/vendor/github.com/
-hack/build-go.sh cmd/hypershift
+go get github.com/maruel/panicparse/stack
 cp -rf $GOPATH/src/github.com/maruel/ $GOPATH/src/github.com/openshift/origin/_output/local/go/src/github.com/openshift/origin/vendor/github.com/
 hack/build-go.sh cmd/hypershift
 sed -i 's/hyperkube controller-manager/hyperkube kube-controller-manager/g' $GOPATH/src/github.com/openshift/origin/hack/local-up-master/lib.sh
